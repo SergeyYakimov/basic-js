23ff1f40606bb559d6add9dce79b061537cb2369
SergeyYakimov <teriertis@yandex.ru>
2021-03-10 18:46:47 +0800

    feat: make 1 task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -2,7 +2,11 @@ const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
   constructor(val) {
-      this.type = val ? 'direct' : 'reverse';
+    this.type = 'direct';
+
+    if (val === false) {
+      this.type = 'reverse';
+    }
   }
 
   encrypt(message, key) {
@@ -10,24 +14,68 @@ class VigenereCipheringMachine {
       throw new Error();
     }
 
-    // const resultArr = [];
+    let result = [];
+    const messageStr = message.toLowerCase();
+    const keyStr = key.toLowerCase();
+    let offset = 0;
 
-    // if (message.length > key.length) {
-    //   for (let i = 0; i < (message.length - key.length); i++) {
-    //     message = message + key.push(key[i]);
-    //   }
-    // }
+    for (let i = 0; i < messageStr.length; i++) {
+      if (messageStr.charCodeAt(i) < 97 || messageStr.charCodeAt(i) > 122) {
+        result[i] = messageStr[i];
+        offset++;
+      } else {
+          let code = (messageStr.charCodeAt(i) + keyStr.charCodeAt(Math.abs((keyStr.length + i - offset) % keyStr.length))) - 97;
+      
+          if (code > 122) {
+            code = code - 26;
+          }
 
-    // for (let j = 0; j < messageArr.length; j++) {
-    //   message.charCodeAt(j) + 
-    //   resultArr.push()
-    // }
+          result[i] = String.fromCharCode(code);
+      }
+    }
+
+    result = result.join('');
+
+    if (this.type === 'reverse') {
+      result = result.split('').reverse().join('');
+    }
+
+    return result.toUpperCase();
+
   }
 
   decrypt(encryptedMessage, key) {
     if (!encryptedMessage || !key) {
       throw new Error();
     }
+
+    let result = [];
+    const messageStr = encryptedMessage.toLowerCase();
+    const keyStr = key.toLowerCase();
+    let offset = 0;
+
+    for (let i = 0; i < messageStr.length; i++) {
+      if (messageStr.charCodeAt(i) < 97 || messageStr.charCodeAt(i) > 122) {
+        result[i] = messageStr[i];
+        offset++;
+      } else {
+          let code = (messageStr.charCodeAt(i) - keyStr.charCodeAt(Math.abs((keyStr.length + i - offset) % keyStr.length))) + 97;
+      
+          if (code < 97) {
+            code = code + 26;
+          }
+
+          result[i] = String.fromCharCode(code);
+      }
+    }
+    
+    result = result.join('');
+
+    if (this.type === 'reverse') {
+      result = result.split('').reverse().join('');
+    }
+
+    return result.toUpperCase();
   }
 }
 
