6d1199348c7174b8a538a2b4deb1accd9ad3f4ac
SergeyYakimov <teriertis@yandex.ru>
2021-03-10 22:02:20 +0800

    feat: make 1 task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,66 +1,62 @@
-const CustomError = require('../extensions/custom-error');
+const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/*arr*/) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-  // if (!Array.isArray(arr)) {
-  //   throw new Error();
-  // }
+module.exports = function transform(arr) {
 
-  // let controlSequences = [];
-  // const newArr = [...arr];
-
-  // arr.forEach((item, i) => {
-  //   if (typeof item === 'string') {
-  //     controlSequences.push({ control: item, pos: i });
-  //   }
-  // });
-
-  // controlSequences.forEach(({ control, pos }) => {
-  //   if (control === '--discard-next' && newArr[pos + 1]) {
-  //     delete newArr[pos];
-  //     delete newArr[pos + 1];
-  //   } else if (control === '--discard-next' && !newArr[pos + 1]) {
-  //     delete newArr[pos];
-  //   } else if (control === '--discard-prev' && newArr[pos - 1]) {
-  //     delete newArr[pos];
-  //     delete newArr[pos - 1];
-  //   } else if (control === '--discard-prev' && !newArr[pos - 1]) {
-  //     delete newArr[pos];
-  //   } else if (control === '--double-next' && newArr[pos + 1]) {
-  //     newArr.splice(newArr[pos], 0, newArr[pos + 1]);
-  //     delete newArr[pos];
-  //   } else if (control === '--double-next' && !newArr[pos + 1]) {
-  //     delete newArr[pos];
-  //   } else if (control === '--double-prev' && newArr[pos - 1]) {
-  //     newArr.splice(newArr[pos], 0, newArr[pos - 1]);
-  //     delete newArr[pos];
-  //   } else if (control === '--double-prev' && !newArr[pos - 1]) {
-  //     delete newArr[pos];
-  //   }
-  // });
-
-  // return newArr.filter((x) => x !== undefined);
-  /*const newArr = [];
-  let a = 1;
-
-  for (let i = 0; i < arr.length; i = i + a) {
-    if (typeof arr[i] !== 'string') {
-      newArr.push(arr[i]);
-      a = 1;
-    } else if (arr[i] === '--discard-next') {
-      a = 2;
-    } else if (arr[i] === '--discard-prev' && newArr.length) {
-      newArr.pop();
-      a = 1;
-    } else if (arr[i] === '--double-prev' && newArr.length && arr[i - 1]) {
-      newArr.push(arr[i - 1]);
-      a = 1;
-    } else if (arr[i] === '--double-next') {
-      newArr.push(arr[i + 1]);
-      a = 1;
+  if (!Array.isArray(arr)) {
+    throw new Error();
+  }
+    
+  const newArr = [...arr];
+  
+  for (let i = 0; i < newArr.length; i++) { 
+    if (typeof newArr[i] !== 'string') {
+      continue;
+    }  
+       
+    if (newArr[i] === '--discard-next') {
+        
+      if ((typeof(newArr[i + 1]) == 'undefined' || newArr.length === 1)){
+        newArr.splice(i, 1);
+        i--;
+      } else if ((newArr[i + 2] === '--double-prev') || (newArr[i + 2] === '--discard-prev')) {         
+        newArr.splice(i, 3);
+        i--;
+      } else {
+        newArr.splice(i, 2);
+        i--;
+      }          
+    }
+     
+    if (newArr[i] === '--double-next') {
+      if (((typeof (newArr[i+1])) == 'undefined') || (newArr.length == 1) ){
+        newArr.splice(i, 1);
+        i--;
+      } else {
+        newArr.splice(i, 1, newArr[i+1]);   
+      }     
+    }
+    
+    if (newArr[i] === '--discard-prev') {
+            
+      if (i === 0) {
+        newArr.splice(0, 1);  
+        i--;
+      } else {
+        newArr.splice(i - 1, 2); 
+        i -= 2;  
+      } 
+    }
+    
+    if (newArr[i] === '--double-prev') {
+            
+      if (i === 0) {
+        newArr.splice(0, 1); 
+        i--;        
+      } else {
+        newArr.splice(i, 1, newArr[i-1]);  
+      }  
     }
   }
-
-  return newArr;*/
+    
+  return newArr;  
 };
